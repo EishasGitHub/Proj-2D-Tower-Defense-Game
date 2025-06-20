06e2069e0a9574b9fa227e09554cea9ada9297b9
EishasGitHub <eishaetc@gmail.com>
2025-06-20 23:36:30 +0500

    Fully Functional Main Menu added

---
+++ 2d-tower-defense-game/scripts/main_menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var credits = $CanvasLayer/Credits
+
+func _on_start_button_pressed() -> void:
+	print("Starting game...")
+	#get_tree().change_scene_to_file("")
+
+
+func _on_credits_button_pressed() -> void:
+	credits.popup_centered()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
